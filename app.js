5b2d668b821d2842f14ea1176ec64f9d04cadf75
STUDIO SEMANTICA <m90studio@tutanota.com>
2020-08-24 16:15:36 -0400

    Login Functionality with Vue

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 const app = new Vue({
     el: "#app",
     data: {
-        loggedin: true,
+        loggedin: false,
         JWT: "",
-        hello: "Hello world"
+        createUN: "",
+        createPW: "",
+        devURL:'http://localhost:3000',
+        prodURL:null,
+        user:null,
+        token:null  
+    },
+methods: {
+
+    handleLogin: function(event){
+        event.preventDefault()
+        const URL = this.prodURL ? this.prodURL : this.devURL
+        console.log(URL)
+        const user = {username: this.createUN, password: this.createPW}
+        console.log(user)
+        fetch(`${URL}/login`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+      })
+      .then(response => response.json())
+      .then(data => {
+          this.user = data.user
+          this.token = data.token
+          this.loggedin = true;
+          this.createPW = ""
+          this.createUN = ""
+    })
+    },
+    handleLogout: function(){
+        this.loggedin = false;
+        this.user=null;
+        this.token=null;
     }
-})
+ }
+});
